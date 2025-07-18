00beb1111c11982c7bcab7d1281ebc41d4d11ba0
morgantstewart <morgan@paysigil.com>
2025-07-18 13:24:35 -0400

    added fruits schema

---
+++ models/fruit.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  isReadyToEat: Boolean,
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema); // create model
+
+
+
+
+
+module.exports = Fruit;
+
--- server.js
+++ server.js
@@ -27,3 +27,15 @@ app.get("/", async (req, res) => {
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
+
+
+// server.js
+
+mongoose.connect(process.env.MONGO_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// Import the Fruit model
+const Fruit = require("./models/fruit.js");
