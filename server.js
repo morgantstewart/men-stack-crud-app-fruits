24254d3e1e0bd773581f09098469aaa5cbf27fd5
morgantstewart <morgan@paysigil.com>
2025-07-18 12:18:06 -0400

    server built

---
+++ server.js
@@ -0,0 +1,19 @@
+// Here is where we import modules
+// We begin by loading Express
+const express = require('express');
+
+const app = express();
+
+
+// server.js
+
+// GET /
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
