aecd6eed0e682ee3c599cbdf0696dd931970f413
morgantstewart <morgan@paysigil.com>
2025-07-18 13:15:43 -0400

    connect to mongodb atlas

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-// Here is where we import modules
-// We begin by loading Express
-const express = require('express');
+//server.js
 
+const dotenv = require("dotenv"); // require package
+
+dotenv.config(); // Loads the environment variables from .env file
+
+const mongoose = require("mongoose"); // require package
+const express = require('express');
 const app = express();
+
+
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
 
 
 
 // GET /
 app.get("/", async (req, res) => {
-  res.render('index.ejs');
+  res.render("index.ejs");
 });
-
 
 
 app.listen(3000, () => {
