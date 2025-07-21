7df72e937d0075f5cc2e18ab95bebb659701e41a
morgantstewart <morgan@paysigil.com>
2025-07-21 10:55:17 -0400

    fixed some bugs this AM

--- server.js
+++ server.js
@@ -12,13 +12,15 @@ const app = express();
 const Fruit = require("./models/fruit.js");
 
 
+
 mongoose.connect(process.env.MONGODB_URI);
-// log connection status to terminal on start
+
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-
+app.use(express.urlencoded({ extended: false }));
+//middleware in front of routes
 
 
 // GET /
@@ -27,16 +29,26 @@ app.get("/", async (req, res) => {
 });
 
 
+//GET fruits/new
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
+
+
+//POST /fruits
+app.post('/fruits', async (req,res) => {
+  if (req.body.isReadyToEat === 'on') {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  
+
+  await Fruit.create.apply(req.body);
+  res.redirect('/fruits/new');
+});
+
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
-
-
-// server.js
-
-mongoose.connect(process.env.MONGO_URI);
-
-mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
-
