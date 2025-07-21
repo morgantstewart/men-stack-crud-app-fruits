1851c53c51aa34daa711b42564f3db027397cab6
morgantstewart <morgan@paysigil.com>
2025-07-21 11:15:35 -0400

    connected form

--- server.js
+++ server.js
@@ -29,6 +29,20 @@ app.get("/", async (req, res) => {
 });
 
 
+// GET /fruits
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+  res.render("fruits/index.ejs", { fruits: allFruits });
+});
+
+
+
+
+
+
+
+
+
 //GET fruits/new
 app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs");
@@ -36,18 +50,27 @@ app.get("/fruits/new", (req, res) => {
 
 
 //POST /fruits
-app.post('/fruits', async (req,res) => {
-  if (req.body.isReadyToEat === 'on') {
+// app.post('/fruits', async (req,res) => {
+//   if (req.body.isReadyToEat === 'on') {
+//     req.body.isReadyToEat = true;
+//   } else {
+//     req.body.isReadyToEat = false;
+//   }
+  
+
+//   await Fruit.create.apply(req.body);
+//   res.redirect('/fruits/new');
+// });
+
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
     req.body.isReadyToEat = true;
   } else {
     req.body.isReadyToEat = false;
   }
-  
-
-  await Fruit.create.apply(req.body);
-  res.redirect('/fruits/new');
+  await Fruit.create(req.body);
+  res.redirect("/fruits/new");
 });
-
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
